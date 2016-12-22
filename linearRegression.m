857ed7d6adc348b76ae8e56cef2506474bcba126
mantofast <mantofast@qq.com>
2016-12-22 20:55:12 +0800

    FIX dimension erro

--- linearRegression.m
+++ linearRegression.m
@@ -13,23 +13,27 @@ n=length(x(1,:));
 theta=ones(n+1,1);
 m=length(y);
 x0=ones(m,1);
-x=[x0,x];
+X=[x0,x];
 alpha=0.1;
 delta=1;
 while(delta>0.00001)
     delta=0;
     for i=1:m
-        for j= 1:n
-          theta(j)=theta(j)-alpha*(theta'*x(i,:)'-y(i))*x(i,j);
+        for j= 1:n+1
+          theta(j)=theta(j)-alpha*(theta'*X(i,:)'-y(i))*X(i,j);
         end
     end
     %cost function, erro
     for i=1:m
-      delta=+(theta'*x(i,:)'-y(i))^2/2/m;
+      delta=+(theta'*X(i,:)'-y(i))^2/2/m;
     end
    % delta;
 end
+
 theta'*[1,z]'
+plot(x,y,'r')
+hold on
+plot(x,X*theta,'b')
 
 
 
